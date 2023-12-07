bb383db81660f29d9d8c13b1d1b6f5db8a123f30
manairaed <manairaed1998@gmail.com>
2023-12-07 23:03:14 +0100

    commit init

---
+++ src/main/java/tn/nuitinfo/nuitinfo/NuitInfoApplication.java
@@ -0,0 +1,13 @@
+package tn.nuitinfo.nuitinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuitInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuitInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/nuitinfo/nuitinfo/NuitInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.nuitinfo.nuitinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuitInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
